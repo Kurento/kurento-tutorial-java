0b0fd709e1579330c5d138501ee6bb37b27fab46
Boni Garcia <bgarcia@gsyc.es>
2014-08-29 14:55:27 +0200

    Changed numeration of tutorials and clean-up
    Change-Id: I7e839a8de8d18b7e1793b1213fce23869adf59ac

--- kurento-one2one-call/src/main/resources/static/js/index.js
+++ kurento-one2one-call/src/main/resources/static/js/index.js
@@ -1,3 +1,18 @@
+/*
+* (C) Copyright 2014 Kurento (http://kurento.org/)
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the GNU Lesser General Public License
+* (LGPL) version 2.1 which accompanies this distribution, and is available at
+* http://www.gnu.org/licenses/lgpl-2.1.html
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+*/
+
 var ws = new WebSocket('ws://' + location.host + '/call');
 var videoInput;
 var videoOutput;
@@ -13,7 +28,7 @@ window.onload = function() {
 
 window.onbeforeunload = function() {
 	ws.close();
-};
+}
 
 ws.onmessage = function(message) {
 	var parsedMessage = JSON.parse(message.data);
