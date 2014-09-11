ce2c6744b0d24ceee47dea3f92ca777ee833f521
Boni Garcia <bgarcia@gsyc.es>
2014-09-11 15:16:50 +0200

    Added explicit onOffer and onError functions to improve readability in hello-world
    Change-Id: Id3ef5de40583b47a588967cd0f9c246cbe545ed2

--- kurento-hello-world/src/resources/static/js/index.js
+++ kurento-hello-world/src/resources/static/js/index.js
@@ -1,17 +1,17 @@
 /*
-* (C) Copyright 2014 Kurento (http://kurento.org/)
-*
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the GNU Lesser General Public License
-* (LGPL) version 2.1 which accompanies this distribution, and is available at
-* http://www.gnu.org/licenses/lgpl-2.1.html
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*
-*/
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
 
 var webRtcPeer;
 var videoInput;
@@ -27,34 +27,38 @@ window.onload = function() {
 function start() {
 	console.log("Starting video call ...");
 	showSpinner(videoInput, videoOutput);
-
-	webRtcPeer = kurentoUtils.WebRtcPeer.startSendRecv(videoInput, videoOutput, function(offerSdp, wp) {
-		console.info('Invoking SDP offer callback function ' + location.host);
-		$.ajax({
-			url : location.protocol + '/helloworld',
-			type : 'POST',
-			dataType : 'text',
-			contentType : 'application/sdp',
-			data : offerSdp,
-			success : function(data) {
-				console.log("Received sdpAnswer from server. Processing ...");
-				wp.processSdpAnswer(data);
-			},
-			error : function(jqXHR, textStatus, error) {
-				console.error(error);
-			}
-		});
-	});
+	webRtcPeer = kurentoUtils.WebRtcPeer.startSendRecv(videoInput, videoOutput, onOffer, onError);
 }
 
 function stop() {
-	console.log("Stopping video call ...");
 	if (webRtcPeer) {
+		console.log("Stopping video call ...");
 		webRtcPeer.dispose();
+		webRtcPeer = null;
 	}
-	videoInput.src = '';
-	videoOutput.src = '';
 	hideSpinner(videoInput, videoOutput);
+}
+
+function onOffer(sdpOffer) {
+	console.info('Invoking SDP offer callback function ' + location.host);
+	$.ajax({
+		url : location.protocol + '/helloworld',
+		type : 'POST',
+		dataType : 'text',
+		contentType : 'application/sdp',
+		data : sdpOffer,
+		success : function(sdpAnswer) {
+			console.log("Received sdpAnswer from server. Processing ...");
+			webRtcPeer.processSdpAnswer(sdpAnswer);
+		},
+		error : function(jqXHR, textStatus, error) {
+			onError(error);
+		}
+	});
+}
+
+function onError(error) {
+	console.error(error);
 }
 
 function showSpinner() {
@@ -66,11 +70,15 @@ function showSpinner() {
 
 function hideSpinner() {
 	for (var i = 0; i < arguments.length; i++) {
+		arguments[i].src = '';
 		arguments[i].poster = './img/webrtc.png';
 		arguments[i].style.background = '';
 	}
 }
 
+/**
+ * Lightbox utility (to display media pipeline image in a modal dialog)
+ */
 $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
 	event.preventDefault();
 	$(this).ekkoLightbox();
