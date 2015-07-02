3fd1616b5e5f4757e1b1d5d7ac4fe24590d3c5f8
Boni Garcia <bgarcia@gsyc.es>
2015-07-02 18:20:47 +0200

    [kurento-hello-world] Changed signaling transport from REST to WebSocket
    Change-Id: I1f87c65a155887d6be6e1233736a7778469d9b32

--- kurento-hello-world/src/main/java/org/kurento/tutorial/helloworld/HelloWorldApp.java
+++ kurento-hello-world/src/main/java/org/kurento/tutorial/helloworld/HelloWorldApp.java
@@ -18,7 +18,10 @@ import org.kurento.client.KurentoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 /**
  * Hello World (WebRTC in loobpack) main class.
@@ -26,16 +29,26 @@ import org.springframework.context.annotation.ComponentScan;
  * @author Boni Garcia (bgarcia@gsyc.es)
  * @since 5.0.0
  */
-@ComponentScan
+@Configuration
+@EnableWebSocket
 @EnableAutoConfiguration
-public class HelloWorldApp {
+public class HelloWorldApp implements WebSocketConfigurer {
 
 	final static String DEFAULT_KMS_WS_URI = "ws://localhost:8888/kurento";
 
 	@Bean
+	public HelloWorldHandler handler() {
+		return new HelloWorldHandler();
+	}
+
+	@Bean
 	public KurentoClient kurentoClient() {
-		return KurentoClient.create(System.getProperty("kms.ws.uri",
-				DEFAULT_KMS_WS_URI));
+		return KurentoClient.create(System.getProperty("kms.ws.uri", DEFAULT_KMS_WS_URI));
+	}
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(handler(), "/helloworld");
 	}
 
 	public static void main(String[] args) throws Exception {
