14b39ea377cd8a2b32c07c0b96dbd6bd6e463729
Boni Garcia <bgarcia@gsyc.es>
2015-07-02 18:32:15 +0200

    [kurento-hello-world] Changed local console to bower dependency
    Change-Id: I761cef6020bc25b793d4b5a7fca321661c67ff0e

--- kurento-magic-mirror/src/main/resources/static/js/index.js
+++ kurento-magic-mirror/src/main/resources/static/js/index.js
@@ -24,8 +24,8 @@ const I_CAN_STOP = 1;
 const I_AM_STARTING = 2;
 
 window.onload = function() {
+	console = new Console();
 	console.log("Page loaded ...");
-	console = new Console('console', console);
 	videoInput = document.getElementById('videoInput');
 	videoOutput = document.getElementById('videoOutput');
 	setState(I_CAN_START);
@@ -50,13 +50,13 @@ ws.onmessage = function(message) {
 		onError("Error message from server: " + parsedMessage.message);
 		break;
 	case 'iceCandidate':
-	    webRtcPeer.addIceCandidate(parsedMessage.candidate, function (error) {
+		webRtcPeer.addIceCandidate(parsedMessage.candidate, function(error) {
 			if (error) {
 				console.error("Error adding candidate: " + error);
 				return;
 			}
-	    });
-	    break;
+		});
+		break;
 	default:
 		if (state == I_AM_STARTING) {
 			setState(I_CAN_START);
@@ -73,22 +73,23 @@ function start() {
 
 	console.log("Creating WebRtcPeer and generating local sdp offer ...");
 
-    var options = {
-	      localVideo: videoInput,
-	      remoteVideo: videoOutput,
-	      onicecandidate: onIceCandidate
-	    }
+	var options = {
+		localVideo : videoInput,
+		remoteVideo : videoOutput,
+		onicecandidate : onIceCandidate
+	}
 	webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options,
-		function (error) {
-	      if (error) {
-		      return console.error(error);
-		  }
-		  webRtcPeer.generateOffer(onOffer);
-		});
+			function(error) {
+				if (error) {
+					return console.error(error);
+				}
+				webRtcPeer.generateOffer(onOffer);
+			});
 }
 
 function onOffer(error, offerSdp) {
-	if (error) return console.error ("Error generating the offer");
+	if (error)
+		return console.error("Error generating the offer");
 	console.info('Invoking SDP offer callback function ' + location.host);
 	var message = {
 		id : 'start',
@@ -102,21 +103,22 @@ function onError(error) {
 }
 
 function onIceCandidate(candidate) {
-	  console.log("Local candidate" + JSON.stringify(candidate));
+	console.log("Local candidate" + JSON.stringify(candidate));
 
-	  var message = {
-	    id: 'onIceCandidate',
-	    candidate: candidate
-	  };
-	  sendMessage(message);
+	var message = {
+		id : 'onIceCandidate',
+		candidate : candidate
+	};
+	sendMessage(message);
 }
 
 function startResponse(message) {
 	setState(I_CAN_STOP);
 	console.log("SDP answer received from server. Processing ...");
 
-	webRtcPeer.processAnswer (message.sdpAnswer, function (error) {
-		if (error) return console.error (error);
+	webRtcPeer.processAnswer(message.sdpAnswer, function(error) {
+		if (error)
+			return console.error(error);
 	});
 }
 
@@ -139,17 +141,22 @@ function setState(nextState) {
 	switch (nextState) {
 	case I_CAN_START:
 		$('#start').attr('disabled', false);
+		$("#start").attr('onclick', 'start()');
 		$('#stop').attr('disabled', true);
+		$("#stop").removeAttr('onclick');
 		break;
 
 	case I_CAN_STOP:
 		$('#start').attr('disabled', true);
 		$('#stop').attr('disabled', false);
+		$("#stop").attr('onclick', 'stop()');
 		break;
 
 	case I_AM_STARTING:
 		$('#start').attr('disabled', true);
+		$("#start").removeAttr('onclick');
 		$('#stop').attr('disabled', true);
+		$("#stop").removeAttr('onclick');
 		break;
 
 	default:
