e6bbb30b9394b5722ea10ab43c776cb10c35949a
Boni Garcia <bgarcia@gsyc.es>
2015-07-02 18:20:47 +0200

    [kurento-hello-world] Changed signaling transport from REST to WebSocket
    Change-Id: I1f87c65a155887d6be6e1233736a7778469d9b32

---
+++ kurento-hello-world/src/main/java/org/kurento/tutorial/helloworld/UserSession.java
@@ -0,0 +1,57 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package org.kurento.tutorial.helloworld;
+
+import org.kurento.client.IceCandidate;
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.WebRtcEndpoint;
+
+/**
+ * User session.
+ * 
+ * @author David Fernandez (d.fernandezlop@gmail.com)
+ * @since 6.0.0
+ */
+public class UserSession {
+	private WebRtcEndpoint webRtcEndpoint;
+	private MediaPipeline mediaPipeline;
+
+	public UserSession() {
+	}
+
+	public WebRtcEndpoint getWebRtcEndpoint() {
+		return webRtcEndpoint;
+	}
+
+	public void setWebRtcEndpoint(WebRtcEndpoint webRtcEndpoint) {
+		this.webRtcEndpoint = webRtcEndpoint;
+	}
+
+	public MediaPipeline getMediaPipeline() {
+		return mediaPipeline;
+	}
+
+	public void setMediaPipeline(MediaPipeline mediaPipeline) {
+		this.mediaPipeline = mediaPipeline;
+	}
+
+	public void addCandidate(IceCandidate i) {
+		webRtcEndpoint.addIceCandidate(i);
+	}
+
+	public void release() {
+		this.mediaPipeline.release();
+	}
+}
--- kurento-hello-world/src/main/resources/static/js/index.js
+++ kurento-hello-world/src/main/resources/static/js/index.js
@@ -13,64 +13,161 @@
  *
  */
 
-var webRtcPeer;
+var ws = new WebSocket('ws://' + location.host + '/helloworld');
 var videoInput;
 var videoOutput;
+var webRtcPeer;
+var state = null;
+
+const I_CAN_START = 0;
+const I_CAN_STOP = 1;
+const I_AM_STARTING = 2;
 
 window.onload = function() {
-	console = new Console('console', console);
+	console = new Console();
+	console.log("Page loaded ...");
 	videoInput = document.getElementById('videoInput');
 	videoOutput = document.getElementById('videoOutput');
-	console.log('Page loaded');
+	setState(I_CAN_START);
+}
+
+window.onbeforeunload = function() {
+	ws.close();
+}
+
+ws.onmessage = function(message) {
+	var parsedMessage = JSON.parse(message.data);
+	console.info('Received message: ' + message.data);
+
+	switch (parsedMessage.id) {
+	case 'startResponse':
+		startResponse(parsedMessage);
+		break;
+	case 'error':
+		if (state == I_AM_STARTING) {
+			setState(I_CAN_START);
+		}
+		onError("Error message from server: " + parsedMessage.message);
+		break;
+	case 'iceCandidate':
+	    webRtcPeer.addIceCandidate(parsedMessage.candidate, function (error) {
+			if (error) {
+				console.error("Error adding candidate: " + error);
+				return;
+			}
+	    });
+	    break;
+	default:
+		if (state == I_AM_STARTING) {
+			setState(I_CAN_START);
+		}
+		onError('Unrecognized message', parsedMessage);
+	}
 }
 
 function start() {
-	console.log('Starting video call ...');
+	console.log("Starting video call ...")
+	// Disable start button
+	setState(I_AM_STARTING);
 	showSpinner(videoInput, videoOutput);
 
-	var options = {
-		localVideo : videoInput,
-		remoteVideo : videoOutput,
-		oncandidategatheringdone : onCandidateGatheringDone
-	}
+	console.log("Creating WebRtcPeer and generating local sdp offer ...");
 
+    var options = {
+	      localVideo: videoInput,
+	      remoteVideo: videoOutput,
+	      onicecandidate: onIceCandidate
+	    }
 	webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options,
-			function(error) {
-				if (error) return console.error(error);
-				webRtcPeer.generateOffer(onOffer);
-			});
+		function (error) {
+	      if (error) {
+		      return console.error(error);
+		  }
+		  webRtcPeer.generateOffer(onOffer);
+		});
+}
+
+function onOffer(error, offerSdp) {
+	if (error) return console.error ("Error generating the offer");
+	console.info('Invoking SDP offer callback function ' + location.host);
+	var message = {
+		id : 'start',
+		sdpOffer : offerSdp
+	}
+	sendMessage(message);
+}
+
+function onError(error) {
+	console.error(error);
+}
+
+function onIceCandidate(candidate) {
+	  console.log("Local candidate" + JSON.stringify(candidate));
+
+	  var message = {
+	    id: 'onIceCandidate',
+	    candidate: candidate
+	  };
+	  sendMessage(message);
+}
+
+function startResponse(message) {
+	setState(I_CAN_STOP);
+	console.log("SDP answer received from server. Processing ...");
+
+	webRtcPeer.processAnswer (message.sdpAnswer, function (error) {
+		if (error) return console.error (error);
+	});
 }
 
 function stop() {
+	console.log("Stopping video call ...");
+	setState(I_CAN_START);
 	if (webRtcPeer) {
-		console.log('Stopping video call ...');
 		webRtcPeer.dispose();
 		webRtcPeer = null;
+
+		var message = {
+			id : 'stop'
+		}
+		sendMessage(message);
 	}
 	hideSpinner(videoInput, videoOutput);
 }
 
-function onCandidateGatheringDone() {
-	$.ajax({
-		url : location.protocol + '/helloworld',
-		type : 'POST',
-		dataType : 'text',
-		contentType : 'application/sdp',
-		data : webRtcPeer.getLocalSessionDescriptor().sdp,
-		success : function(sdpAnswer) {
-			console.log("Received sdpAnswer from server. Processing ...");
-			webRtcPeer.processAnswer(sdpAnswer, function(error) {
-				if (error) return console.error(error);
-			});
-		},
-		error : function(jqXHR, textStatus, error) {
-			console.error(error);
-		}
-	});
+function setState(nextState) {
+	switch (nextState) {
+	case I_CAN_START:
+		$('#start').attr('disabled', false);
+		$("#start").attr('onclick', 'start()');
+		$('#stop').attr('disabled', true);
+		$("#stop").removeAttr('onclick');
+		break;
+
+	case I_CAN_STOP:
+		$('#start').attr('disabled', true);
+		$('#stop').attr('disabled', false);
+		$("#stop").attr('onclick', 'stop()');
+		break;
+
+	case I_AM_STARTING:
+		$('#start').attr('disabled', true);
+		$("#start").removeAttr('onclick');
+		$('#stop').attr('disabled', true);
+		$("#stop").removeAttr('onclick');
+		break;
+
+	default:
+		onError("Unknown state " + nextState);
+		return;
+	}
+	state = nextState;
 }
 
-function onOffer(error, sdpOffer) {
-	console.info('Waiting for ICE candidates ...');
+function sendMessage(message) {
+	var jsonMessage = JSON.stringify(message);
+	console.log('Senging message: ' + jsonMessage);
+	ws.send(jsonMessage);
 }
 
 function showSpinner() {
@@ -87,7 +184,6 @@ function hideSpinner() {
 		arguments[i].style.background = '';
 	}
 }
-
 /**
  * Lightbox utility (to display media pipeline image in a modal dialog)
  */
