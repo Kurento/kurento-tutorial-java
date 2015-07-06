a1ce5e580675b849af6caf3ff701a6aaab43d14e
Boni Garcia <bgarcia@gsyc.es>
2015-07-06 13:14:27 +0200

    [kurento-hello-world] Code cleanup for doc
    Change-Id: I047520eeed95cde86383c985d4d76cca65cf3967

--- kurento-hello-world/src/main/resources/static/js/index.js
+++ kurento-hello-world/src/main/resources/static/js/index.js
@@ -25,7 +25,7 @@ const I_AM_STARTING = 2;
 
 window.onload = function() {
 	console = new Console();
-	console.log("Page loaded ...");
+	console.log('Page loaded ...');
 	videoInput = document.getElementById('videoInput');
 	videoOutput = document.getElementById('videoOutput');
 	setState(I_CAN_START);
@@ -47,16 +47,14 @@ ws.onmessage = function(message) {
 		if (state == I_AM_STARTING) {
 			setState(I_CAN_START);
 		}
-		onError("Error message from server: " + parsedMessage.message);
+		onError('Error message from server: ' + parsedMessage.message);
 		break;
 	case 'iceCandidate':
-	    webRtcPeer.addIceCandidate(parsedMessage.candidate, function (error) {
-			if (error) {
-				console.error("Error adding candidate: " + error);
-				return;
-			}
-	    });
-	    break;
+		webRtcPeer.addIceCandidate(parsedMessage.candidate, function(error) {
+			if (error)
+				return console.error('Error adding candidate: ' + error);
+		});
+		break;
 	default:
 		if (state == I_AM_STARTING) {
 			setState(I_CAN_START);
@@ -66,29 +64,30 @@ ws.onmessage = function(message) {
 }
 
 function start() {
-	console.log("Starting video call ...")
+	console.log('Starting video call ...');
+
 	// Disable start button
 	setState(I_AM_STARTING);
 	showSpinner(videoInput, videoOutput);
 
-	console.log("Creating WebRtcPeer and generating local sdp offer ...");
+	console.log('Creating WebRtcPeer and generating local sdp offer ...');
 
-    var options = {
-	      localVideo: videoInput,
-	      remoteVideo: videoOutput,
-	      onicecandidate: onIceCandidate
-	    }
+	var options = {
+		localVideo : videoInput,
+		remoteVideo : videoOutput,
+		onicecandidate : onIceCandidate
+	}
 	webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options,
-		function (error) {
-	      if (error) {
-		      return console.error(error);
-		  }
-		  webRtcPeer.generateOffer(onOffer);
-		});
+			function(error) {
+				if (error)
+					return console.error(error);
+				webRtcPeer.generateOffer(onOffer);
+			});
 }
 
 function onOffer(error, offerSdp) {
-	if (error) return console.error ("Error generating the offer");
+	if (error)
+		return console.error('Error generating the offer');
 	console.info('Invoking SDP offer callback function ' + location.host);
 	var message = {
 		id : 'start',
@@ -102,26 +101,27 @@ function onError(error) {
 }
 
 function onIceCandidate(candidate) {
-	  console.log("Local candidate" + JSON.stringify(candidate));
+	console.log('Local candidate' + JSON.stringify(candidate));
 
-	  var message = {
-	    id: 'onIceCandidate',
-	    candidate: candidate
-	  };
-	  sendMessage(message);
+	var message = {
+		id : 'onIceCandidate',
+		candidate : candidate
+	};
+	sendMessage(message);
 }
 
 function startResponse(message) {
 	setState(I_CAN_STOP);
-	console.log("SDP answer received from server. Processing ...");
+	console.log('SDP answer received from server. Processing ...');
 
-	webRtcPeer.processAnswer (message.sdpAnswer, function (error) {
-		if (error) return console.error (error);
+	webRtcPeer.processAnswer(message.sdpAnswer, function(error) {
+		if (error)
+			return console.error(error);
 	});
 }
 
 function stop() {
-	console.log("Stopping video call ...");
+	console.log('Stopping video call ...');
 	setState(I_CAN_START);
 	if (webRtcPeer) {
 		webRtcPeer.dispose();
@@ -139,26 +139,26 @@ function setState(nextState) {
 	switch (nextState) {
 	case I_CAN_START:
 		$('#start').attr('disabled', false);
-		$("#start").attr('onclick', 'start()');
+		$('#start').attr('onclick', 'start()');
 		$('#stop').attr('disabled', true);
-		$("#stop").removeAttr('onclick');
+		$('#stop').removeAttr('onclick');
 		break;
 
 	case I_CAN_STOP:
 		$('#start').attr('disabled', true);
 		$('#stop').attr('disabled', false);
-		$("#stop").attr('onclick', 'stop()');
+		$('#stop').attr('onclick', 'stop()');
 		break;
 
 	case I_AM_STARTING:
 		$('#start').attr('disabled', true);
-		$("#start").removeAttr('onclick');
+		$('#start').removeAttr('onclick');
 		$('#stop').attr('disabled', true);
-		$("#stop").removeAttr('onclick');
+		$('#stop').removeAttr('onclick');
 		break;
 
 	default:
-		onError("Unknown state " + nextState);
+		onError('Unknown state ' + nextState);
 		return;
 	}
 	state = nextState;
