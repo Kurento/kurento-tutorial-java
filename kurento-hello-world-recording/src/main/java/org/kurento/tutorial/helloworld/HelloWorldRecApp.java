0e969d10357b85e127f204e3916c8b767ce23b01
Radu Tom Vlad <rvlad@naevatec.com>
2015-09-04 14:11:26 +0200

    [kurento-hello-world-recording] Initial commit
    First version of a 'hello world'-ish recording media project.
    Uses kurento repository (which can use MongoDB or disk storage).
    Can also save recorded streams to a local file, depending on the
    URI provided to the RecorderEndpoint.
    
    Change-Id: Id807f5601fabd1e7e5a2843bcaad44a473c04d3f

---
+++ kurento-hello-world-recording/src/main/java/org/kurento/tutorial/helloworld/HelloWorldRecApp.java
@@ -0,0 +1,80 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU Lesser General Public License (LGPL)
+ * version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package org.kurento.tutorial.helloworld;
+
+import org.kurento.client.KurentoClient;
+import org.kurento.commons.PropertiesManager;
+import org.kurento.repository.rest.RepositoryRestApi;
+import org.kurento.repository.rest.RestServiceProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * Hello World (WebRTC in loopback with recording) main class.
+ * 
+ * @author Boni Garcia (bgarcia@gsyc.es)
+ * @author Radu Tom Vlad (rvlad@naevatec.com)
+ * @since 6.1.1
+ */
+@Configuration
+@EnableWebSocket
+@EnableAutoConfiguration
+public class HelloWorldRecApp implements WebSocketConfigurer {
+
+	final static String DEFAULT_KMS_WS_URI = "ws://localhost:8888/kurento";
+	final static String DEFAULT_REPOSITORY_SERVER_URI = "http://localhost:7676";
+
+	final static String REPOSITORY_SERVER_URI = System.getProperty(
+			"repository.uri", DEFAULT_REPOSITORY_SERVER_URI);
+	final static String KMS_WS_URI = System.getProperty("kms.ws.uri",
+			DEFAULT_KMS_WS_URI);
+
+	@Bean
+	public HelloWorldRecHandler handler() {
+		return new HelloWorldRecHandler();
+	}
+
+	@Bean
+	public KurentoClient kurentoClient() {
+		return KurentoClient.create(PropertiesManager.getProperty("kms.ws.uri",
+				KMS_WS_URI));
+	}
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(handler(), "/helloworld");
+	}
+
+	@Bean
+	public RepositoryRestApi repositoryServiceProvider() {
+		if (REPOSITORY_SERVER_URI.startsWith("file://"))
+			return null;
+		return RestServiceProvider.create(REPOSITORY_SERVER_URI)
+				.getRestService();
+	}
+
+	@Bean
+	public UserRegistry registry() {
+		return new UserRegistry();
+	}
+
+	public static void main(String[] args) throws Exception {
+		new SpringApplication(HelloWorldRecApp.class).run(args);
+	}
+}
---
+++ kurento-hello-world-recording/src/main/resources/static/js/index.js
@@ -0,0 +1,249 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+var ws = new WebSocket('ws://' + location.host + '/helloworld');
+var videoInput;
+var videoOutput;
+var webRtcPeer;
+var state = null;
+
+const NO_CALL = 0;
+const IN_CALL = 1;
+const POST_CALL = 2;
+const DISABLED = 3;
+const IN_PLAY = 4;
+
+window.onload = function() {
+	console = new Console();
+	console.log('Page loaded ...');
+	videoInput = document.getElementById('videoInput');
+	videoOutput = document.getElementById('videoOutput');
+	setState(NO_CALL);
+}
+
+window.onbeforeunload = function() {
+	ws.close();
+}
+
+function setState(nextState) {
+	switch (nextState) {
+	case NO_CALL:
+		$('#start').attr('disabled', false);
+		$('#stop').attr('disabled', true);
+		$('#play').attr('disabled', true);
+		break;
+	case DISABLED:
+		$('#start').attr('disabled', true);
+		$('#stop').attr('disabled', true);
+		$('#play').attr('disabled', true);
+		break;
+	case IN_CALL:
+		$('#start').attr('disabled', true);
+		$('#stop').attr('disabled', false);
+		$('#play').attr('disabled', true);
+		break;
+	case POST_CALL:
+		$('#start').attr('disabled', false);
+		$('#stop').attr('disabled', true);
+		$('#play').attr('disabled', false);
+		break;
+	case IN_PLAY:
+		$('#start').attr('disabled', true);
+		$('#stop').attr('disabled', false);
+		$('#play').attr('disabled', true);
+		break;	
+	default:
+		onError('Unknown state ' + nextState);
+		return;
+	}
+	state = nextState;
+}
+
+ws.onmessage = function(message) {
+	var parsedMessage = JSON.parse(message.data);
+	console.info('Received message: ' + message.data);
+
+	switch (parsedMessage.id) {
+	case 'startResponse':
+		startResponse(parsedMessage);
+		break;
+	case 'playResponse':
+		playResponse(parsedMessage);
+		break;
+	case 'playEnd':
+		playEnd();
+		break;
+	case 'error':
+		setState(NO_CALL);
+		onError('Error message from server: ' + parsedMessage.message);
+		break;
+	case 'iceCandidate':
+		webRtcPeer.addIceCandidate(parsedMessage.candidate, function(error) {
+			if (error)
+				return console.error('Error adding candidate: ' + error);
+		});
+		break;
+	default:
+		setState(NO_CALL);
+		onError('Unrecognized message', parsedMessage);
+	}
+}
+
+function start() {
+	console.log('Starting video call ...');
+
+	// Disable start button
+	setState(DISABLED);
+	showSpinner(videoInput, videoOutput);
+
+	console.log('Creating WebRtcPeer and generating local sdp offer ...');
+
+	var options = {
+		localVideo : videoInput,
+		remoteVideo : videoOutput,
+		onicecandidate : onIceCandidate
+	}
+	webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options,
+			function(error) {
+				if (error)
+					return console.error(error);
+				webRtcPeer.generateOffer(onOffer);
+			});
+}
+
+function onOffer(error, offerSdp) {
+	if (error)
+		return console.error('Error generating the offer');
+	console.info('Invoking SDP offer callback function ' + location.host);
+	var message = {
+		id : 'start',
+		sdpOffer : offerSdp
+	}
+	sendMessage(message);
+}
+
+function onError(error) {
+	console.error(error);
+}
+
+function onIceCandidate(candidate) {
+	console.log('Local candidate' + JSON.stringify(candidate));
+
+	var message = {
+		id : 'onIceCandidate',
+		candidate : candidate
+	};
+	sendMessage(message);
+}
+
+function startResponse(message) {
+	setState(IN_CALL);
+	console.log('SDP answer received from server. Processing ...');
+
+	webRtcPeer.processAnswer(message.sdpAnswer, function(error) {
+		if (error)
+			return console.error(error);
+	});
+}
+
+function stop() {
+	var stopMessageId = (state == IN_CALL) ? 'stop' : 'stopPlay';
+	console.log('Stopping video while in ' + state + '...');
+	setState(POST_CALL);
+	if (webRtcPeer) {
+		webRtcPeer.dispose();
+		webRtcPeer = null;
+
+		var message = {
+			id : stopMessageId
+		}
+		sendMessage(message);
+	}
+	hideSpinner(videoInput, videoOutput);
+}
+
+function play() {
+	console.log("Starting to play recorded video...");
+
+	// Disable start button
+	setState(DISABLED);
+	showSpinner(videoOutput);
+
+	console.log('Creating WebRtcPeer and generating local sdp offer ...');
+
+	var options = {
+		remoteVideo : videoOutput,
+		onicecandidate : onIceCandidate
+	}
+	webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options,
+			function(error) {
+				if (error)
+					return console.error(error);
+				webRtcPeer.generateOffer(onPlayOffer);
+			});
+}
+
+function onPlayOffer(error, offerSdp) {
+	if (error)
+		return console.error('Error generating the offer');
+	console.info('Invoking SDP offer callback function ' + location.host);
+	var message = {
+		id : 'play',
+		sdpOffer : offerSdp
+	}
+	sendMessage(message);
+}
+
+
+function playResponse(message) {
+	setState(IN_PLAY);
+	webRtcPeer.processAnswer(message.sdpAnswer, function(error) {
+		if (error)
+			return console.error(error);
+	});
+}
+
+function playEnd() {
+	setState(POST_CALL);
+	hideSpinner(videoInput, videoOutput);
+}
+
+function sendMessage(message) {
+	var jsonMessage = JSON.stringify(message);
+	console.log('Senging message: ' + jsonMessage);
+	ws.send(jsonMessage);
+}
+
+function showSpinner() {
+	for (var i = 0; i < arguments.length; i++) {
+		arguments[i].poster = './img/transparent-1px.png';
+		arguments[i].style.background = "center transparent url('./img/spinner.gif') no-repeat";
+	}
+}
+
+function hideSpinner() {
+	for (var i = 0; i < arguments.length; i++) {
+		arguments[i].src = '';
+		arguments[i].poster = './img/webrtc.png';
+		arguments[i].style.background = '';
+	}
+}
+/**
+ * Lightbox utility (to display media pipeline image in a modal dialog)
+ */
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+	event.preventDefault();
+	$(this).ekkoLightbox();
+});
