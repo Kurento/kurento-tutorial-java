f1e2316797d1ee74ec0276ece09695485c459c57
David Fernandez <d.fernandezlop@gmail.com>
2015-10-28 12:11:37 +0100

    [kurento-send-data-channel] Add demo to receive data from data channels
    Change-Id: Ib811907c33da9aa05a85759d0e10d5c77d9efab3

---
+++ kurento-send-data-channel/package/README.md
@@ -0,0 +1,16 @@
+${project.name}
+======================
+
+The ${project.name} shows how to build a WebRTC in loopback with faceoverlay filter application with Kurento.
+
+Installation instructions
+-------------------------
+
+By running install.sh, the jar file containing the demo will be copied into 
+_/var/lib/kurento_, and the startup script will be put in _/etc/init.d/${project.artifactId}_. Once the demo is installed, it can be managed as a regular service with
+
+```
+sudo service ${project.artifactId} {start|stop|restart}
+```
+
+If you want to change the port, you can do so by editing the startup script, and setting the desired value in _APP\_PORT_.
---
+++ kurento-send-data-channel/src/main/java/org/kurento/tutorial/senddatachannel/SendDataChannelApp.java
@@ -0,0 +1,60 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package org.kurento.tutorial.senddatachannel;
+
+import org.kurento.client.KurentoClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * Show Data Channel main class.
+ * 
+ * @author Boni Garcia (bgarcia@gsyc.es)
+ * @author David Fernandez (d.fernandezlop@gmail.com)
+ * @since 6.1.1
+ */
+@Configuration
+@EnableWebSocket
+@EnableAutoConfiguration
+public class SendDataChannelApp implements WebSocketConfigurer {
+
+	final static String DEFAULT_KMS_WS_URI = "ws://localhost:8888/kurento";
+	final static String DEFAULT_APP_SERVER_URL = "http://localhost:8080";
+
+	@Bean
+	public SendDataChannelHandler handler() {
+		return new SendDataChannelHandler();
+	}
+
+	@Bean
+	public KurentoClient kurentoClient() {
+		return KurentoClient.create(System.getProperty("kms.ws.uri",
+				DEFAULT_KMS_WS_URI));
+	}
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(handler(), "/senddatachannel");
+	}
+
+	public static void main(String[] args) throws Exception {
+		new SpringApplication(SendDataChannelApp.class).run(args);
+	}
+}
