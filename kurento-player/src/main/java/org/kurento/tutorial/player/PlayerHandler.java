8a7c603f10ac5b2dded63049f000bdc777b6be71
Boni Garcia <bgarcia@gsyc.es>
2015-10-30 17:44:32 +0100

    Add kurento-player tutorial (PlayerEndpoint -> WebRtcEndpoint)
    Change-Id: I47a07d5f4a7cf8a352be1f9a73ab941b154bb7e8

---
+++ kurento-player/src/main/java/org/kurento/tutorial/player/PlayerHandler.java
@@ -0,0 +1,197 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package org.kurento.tutorial.player;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.kurento.client.EndOfStreamEvent;
+import org.kurento.client.ErrorEvent;
+import org.kurento.client.EventListener;
+import org.kurento.client.IceCandidate;
+import org.kurento.client.KurentoClient;
+import org.kurento.client.OnIceCandidateEvent;
+import org.kurento.jsonrpc.JsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+/**
+ * Protocol handler for video player through WebRTC.
+ * 
+ * @author Boni Garcia (bgarcia@gsyc.es)
+ * @since 6.1.1
+ */
+public class PlayerHandler extends TextWebSocketHandler {
+
+	@Autowired
+	private KurentoClient kurento;
+
+	private final Logger log = LoggerFactory.getLogger(PlayerHandler.class);
+	private final Gson gson = new GsonBuilder().create();
+	private final ConcurrentHashMap<String, PlayerMediaPipeline> pipelines = new ConcurrentHashMap<String, PlayerMediaPipeline>();
+
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message)
+			throws Exception {
+		JsonObject jsonMessage = gson.fromJson(message.getPayload(),
+				JsonObject.class);
+		String sessionId = session.getId();
+		log.debug("Incoming message {} from sessionId", jsonMessage, sessionId);
+
+		try {
+			switch (jsonMessage.get("id").getAsString()) {
+			case "start":
+				start(session, jsonMessage);
+				break;
+			case "stop":
+				stop(sessionId);
+				break;
+			case "pause":
+				pause(sessionId);
+				break;
+			case "resume":
+				resume(sessionId);
+				break;
+			case "onIceCandidate":
+				onIceCandidate(sessionId, jsonMessage);
+				break;
+			default:
+				sendError(session, "Invalid message with id "
+						+ jsonMessage.get("id").getAsString());
+				break;
+			}
+		} catch (Throwable t) {
+			log.error("Exception handling message {} in sessionId {}",
+					jsonMessage, sessionId, t);
+			sendError(session, t.getMessage());
+		}
+	}
+
+	private void start(final WebSocketSession session, JsonObject jsonMessage) {
+		// 1. Media pipeline
+		PlayerMediaPipeline playerMediaPipeline = new PlayerMediaPipeline();
+		playerMediaPipeline.initMediaPipeline(kurento);
+		pipelines.put(session.getId(), playerMediaPipeline);
+
+		// 2. WebRtcEndpoint
+		String sdpOffer = jsonMessage.get("sdpOffer").getAsString();
+		String sdpAnswer = playerMediaPipeline.processOffer(sdpOffer);
+
+		JsonObject response = new JsonObject();
+		response.addProperty("id", "startResponse");
+		response.addProperty("sdpAnswer", sdpAnswer);
+		sendMessage(session, response.toString());
+
+		playerMediaPipeline.gatherCandidates(
+				new EventListener<OnIceCandidateEvent>() {
+					@Override
+					public void onEvent(OnIceCandidateEvent event) {
+						JsonObject response = new JsonObject();
+						response.addProperty("id", "iceCandidate");
+						response.add("candidate",
+								JsonUtils.toJsonObject(event.getCandidate()));
+						sendMessage(session, response.toString());
+					}
+				});
+
+		// 3. PlayEndpoint
+		playerMediaPipeline.play(new EventListener<ErrorEvent>() {
+			@Override
+			public void onEvent(ErrorEvent event) {
+				log.info("ErrorEvent: {}", event.getDescription());
+				sendPlayEnd(session);
+			}
+		}, new EventListener<EndOfStreamEvent>() {
+			@Override
+			public void onEvent(EndOfStreamEvent event) {
+				log.info("EndOfStreamEvent: {}", event.getTimestamp());
+				sendPlayEnd(session);
+			}
+		});
+
+	}
+
+	private void pause(String sessionId) {
+		if (pipelines.containsKey(sessionId)) {
+			pipelines.get(sessionId).pause();
+		}
+	}
+
+	private void resume(String sessionId) {
+		if (pipelines.containsKey(sessionId)) {
+			pipelines.get(sessionId).play();
+		}
+	}
+
+	private void stop(String sessionId) {
+		if (pipelines.containsKey(sessionId)) {
+			pipelines.get(sessionId).release();
+			pipelines.remove(sessionId);
+		}
+	}
+
+	private void onIceCandidate(String sessionId, JsonObject jsonMessage) {
+		if (pipelines.containsKey(sessionId)) {
+			JsonObject jsonCandidate = jsonMessage.get("candidate")
+					.getAsJsonObject();
+			IceCandidate candidate = new IceCandidate(
+					jsonCandidate.get("candidate").getAsString(),
+					jsonCandidate.get("sdpMid").getAsString(),
+					jsonCandidate.get("sdpMLineIndex").getAsInt());
+			pipelines.get(sessionId).addIceCandidate(candidate);
+		}
+	}
+
+	public void sendPlayEnd(WebSocketSession session) {
+		if (pipelines.containsKey(session.getId())) {
+			JsonObject response = new JsonObject();
+			response.addProperty("id", "playEnd");
+			sendMessage(session, response.toString());
+		}
+	}
+
+	private void sendError(WebSocketSession session, String message) {
+		if (pipelines.containsKey(session.getId())) {
+			JsonObject response = new JsonObject();
+			response.addProperty("id", "error");
+			response.addProperty("message", message);
+			sendMessage(session, response.toString());
+		}
+	}
+
+	private synchronized void sendMessage(WebSocketSession session,
+			String message) {
+		try {
+			session.sendMessage(new TextMessage(message));
+		} catch (IOException e) {
+			log.error("Exception sending message", e);
+		}
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session,
+			CloseStatus status) throws Exception {
+		stop(session.getId());
+	}
+}
---
+++ kurento-player/src/main/resources/static/js/index.js
@@ -0,0 +1,253 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+var ws = new WebSocket('ws://' + location.host + '/player');
+var video;
+var webRtcPeer;
+var state = null;
+
+var I_CAN_START = 0;
+var I_CAN_STOP = 1;
+var I_AM_STARTING = 2;
+
+window.onload = function() {
+	console = new Console();
+	video = document.getElementById('video');
+	setState(I_CAN_START);
+}
+
+window.onbeforeunload = function() {
+	ws.close();
+}
+
+ws.onmessage = function(message) {
+	var parsedMessage = JSON.parse(message.data);
+	console.info('Received message: ' + message.data);
+
+	switch (parsedMessage.id) {
+	case 'startResponse':
+		startResponse(parsedMessage);
+		break;
+	case 'error':
+		if (state == I_AM_STARTING) {
+			setState(I_CAN_START);
+		}
+		onError('Error message from server: ' + parsedMessage.message);
+		break;
+	case 'playEnd':
+		playEnd();
+		break;
+	case 'iceCandidate':
+		webRtcPeer.addIceCandidate(parsedMessage.candidate, function(error) {
+			if (error)
+				return console.error('Error adding candidate: ' + error);
+		});
+		break;
+	default:
+		if (state == I_AM_STARTING) {
+			setState(I_CAN_START);
+		}
+		onError('Unrecognized message', parsedMessage);
+	}
+}
+
+function start() {
+	// Disable start button
+	setState(I_AM_STARTING);
+	showSpinner(video);
+
+	var mode = $('input[name="mode"]:checked').val();
+	console
+			.log('Creating WebRtcPeer in " + mode + " mode and generating local sdp offer ...');
+
+	// Video and audio by default
+	var userMediaConstraints = {
+		audio : true,
+		video : true
+	}
+
+	if (mode == 'video-only') {
+		userMediaConstraints.audio = false;
+	} else if (mode == 'audio-only') {
+		userMediaConstraints.video = false;
+	}
+
+	var options = {
+		remoteVideo : video,
+		mediaConstraints : userMediaConstraints,
+		onicecandidate : onIceCandidate
+	}
+
+	console.info('User media constraints' + userMediaConstraints);
+
+	webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options,
+			function(error) {
+				if (error)
+					return console.error(error);
+				webRtcPeer.generateOffer(onOffer);
+			});
+}
+
+function onOffer(error, offerSdp) {
+	if (error)
+		return console.error('Error generating the offer');
+	console.info('Invoking SDP offer callback function ' + location.host);
+	var message = {
+		id : 'start',
+		sdpOffer : offerSdp
+	}
+	sendMessage(message);
+}
+
+function onError(error) {
+	console.error(error);
+}
+
+function onIceCandidate(candidate) {
+	console.log('Local candidate' + JSON.stringify(candidate));
+
+	var message = {
+		id : 'onIceCandidate',
+		candidate : candidate
+	}
+	sendMessage(message);
+}
+
+function startResponse(message) {
+	setState(I_CAN_STOP);
+	console.log('SDP answer received from server. Processing ...');
+
+	webRtcPeer.processAnswer(message.sdpAnswer, function(error) {
+		if (error)
+			return console.error(error);
+	});
+}
+
+function pause() {
+	togglePause()
+	console.log('Pausing video ...');
+	var message = {
+		id : 'pause'
+	}
+	sendMessage(message);
+}
+
+function resume() {
+	togglePause()
+	console.log('Resuming video ...');
+	var message = {
+		id : 'resume'
+	}
+	sendMessage(message);
+}
+
+function stop() {
+	console.log('Stopping video ...');
+	setState(I_CAN_START);
+	if (webRtcPeer) {
+		webRtcPeer.dispose();
+		webRtcPeer = null;
+
+		var message = {
+			id : 'stop'
+		}
+		sendMessage(message);
+	}
+	hideSpinner(video);
+}
+
+function playEnd() {
+	setState(I_CAN_START);
+	hideSpinner(video);
+}
+
+function setState(nextState) {
+	switch (nextState) {
+	case I_CAN_START:
+		enableButton('#start', 'start()');
+		disableButton('#pause');
+		disableButton('#stop');
+		break;
+
+	case I_CAN_STOP:
+		disableButton('#start');
+		enableButton('#pause', 'pause()');
+		enableButton('#stop', 'stop()');
+		break;
+
+	case I_AM_STARTING:
+		disableButton('#start');
+		disableButton('#pause');
+		disableButton('#stop');
+		break;
+
+	default:
+		onError('Unknown state ' + nextState);
+		return;
+	}
+	state = nextState;
+}
+
+function sendMessage(message) {
+	var jsonMessage = JSON.stringify(message);
+	console.log('Senging message: ' + jsonMessage);
+	ws.send(jsonMessage);
+}
+
+function togglePause() {
+	var pauseText = $("#pause-text").text();
+	if (pauseText == " Resume ") {
+		$("#pause-text").text(" Pause ");
+		$("#pause-icon").attr('class', 'glyphicon glyphicon-pause');
+		$("#pause").attr('onclick', "pause()");
+	} else {
+		$("#pause-text").text(" Resume ");
+		$("#pause-icon").attr('class', 'glyphicon glyphicon-play');
+		$("#pause").attr('onclick', "resume()");
+	}
+}
+
+function disableButton(id) {
+	$(id).attr('disabled', true);
+	$(id).removeAttr('onclick');
+}
+
+function enableButton(id, functionName) {
+	$(id).attr('disabled', false);
+	$(id).attr('onclick', functionName);
+}
+
+function showSpinner() {
+	for (var i = 0; i < arguments.length; i++) {
+		arguments[i].poster = './img/transparent-1px.png';
+		arguments[i].style.background = "center transparent url('./img/spinner.gif') no-repeat";
+	}
+}
+
+function hideSpinner() {
+	for (var i = 0; i < arguments.length; i++) {
+		arguments[i].src = '';
+		arguments[i].poster = './img/webrtc.png';
+		arguments[i].style.background = '';
+	}
+}
+
+/**
+ * Lightbox utility (to display media pipeline image in a modal dialog)
+ */
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+	event.preventDefault();
+	$(this).ekkoLightbox();
+});
