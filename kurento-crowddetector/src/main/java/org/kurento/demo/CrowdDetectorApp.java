86070d064ed73f7c3d64fb1a70c15b0581f33666
Ivan Gracia <izanmail@gmail.com>
2015-12-07 16:44:35 +0100

    Removed HTTP connector, and HHTP to HTTPS redirection
    Change-Id: Id4e608bc64d23ea1b2a1e389e17cd2810e3ea596

--- kurento-crowddetector/src/main/java/org/kurento/demo/CrowdDetectorApp.java
+++ kurento-crowddetector/src/main/java/org/kurento/demo/CrowdDetectorApp.java
@@ -15,21 +15,13 @@
 
 package org.kurento.demo;
 
-import static org.kurento.commons.PropertiesManager.getProperty;
-
 import java.io.IOException;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.kurento.client.KurentoClient;
 import org.kurento.orion.OrionConnector;
 import org.kurento.orion.OrionConnectorConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -50,37 +42,6 @@ public class CrowdDetectorApp implements WebSocketConfigurer {
 
   private static final String DEFAULT_KMS_WS_URI = "ws://localhost:8888/kurento";
   static final String DEFAULT_CONFIG_FILE_PATH = "/config/configuration.conf.json";
-
-  private static final int SECURE_PORT = getProperty("ws.secureport", 8443);
-  private static final int PLAIN_PORT = getProperty("ws.port", 8080);
-
-  @Bean
-  public EmbeddedServletContainerFactory servletContainer() {
-    TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-      @Override
-      protected void postProcessContext(Context context) {
-        SecurityConstraint securityConstraint = new SecurityConstraint();
-        securityConstraint.setUserConstraint("CONFIDENTIAL");
-        SecurityCollection collection = new SecurityCollection();
-        collection.addPattern("/*");
-        securityConstraint.addCollection(collection);
-        context.addConstraint(securityConstraint);
-      }
-    };
-
-    tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
-    return tomcat;
-  }
-
-  private Connector initiateHttpConnector() {
-    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-    connector.setScheme("http");
-    connector.setPort(PLAIN_PORT);
-    connector.setSecure(false);
-    connector.setRedirectPort(SECURE_PORT);
-
-    return connector;
-  }
 
   @Bean
   public ConfigurationReader init() throws IOException {
