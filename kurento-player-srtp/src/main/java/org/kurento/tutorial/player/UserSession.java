bf92f45b4ca11f7b91e396ec20c68caeff0f130a
Juan Navarro <juan.navarro@gmx.es>
2017-11-27 21:13:33 +0100

    Add new demo: SRTP Player

---
+++ kurento-player-srtp/src/main/java/org/kurento/tutorial/player/UserSession.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2017 Kurento (https://www.kurento.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kurento.tutorial.player;
+
+import org.kurento.client.IceCandidate;
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.RtpEndpoint;
+import org.kurento.client.WebRtcEndpoint;
+
+public class UserSession {
+
+  private WebRtcEndpoint webRtcEndpoint;
+  private MediaPipeline mediaPipeline;
+  private RtpEndpoint rtpEndpoint;
+
+  public UserSession() {
+  }
+
+  public WebRtcEndpoint getWebRtcEndpoint() {
+    return webRtcEndpoint;
+  }
+
+  public void setWebRtcEndpoint(WebRtcEndpoint webRtcEndpoint) {
+    this.webRtcEndpoint = webRtcEndpoint;
+  }
+
+  public MediaPipeline getMediaPipeline() {
+    return mediaPipeline;
+  }
+
+  public void setMediaPipeline(MediaPipeline mediaPipeline) {
+    this.mediaPipeline = mediaPipeline;
+  }
+
+  public void addCandidate(IceCandidate candidate) {
+    webRtcEndpoint.addIceCandidate(candidate);
+  }
+
+  public RtpEndpoint getRtpEndpoint() {
+    return rtpEndpoint;
+  }
+
+  public void setRtpEndpoint(RtpEndpoint rtpEndpoint) {
+    this.rtpEndpoint = rtpEndpoint;
+  }
+
+  public void release() {
+    this.mediaPipeline.release();
+  }
+}
