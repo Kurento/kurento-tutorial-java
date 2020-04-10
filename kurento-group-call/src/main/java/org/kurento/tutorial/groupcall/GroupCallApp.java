cf955b21da7bff14220e9d28eb6e178c1947e9ee
hgcummings <harry@xlr3.com>
2020-04-10 12:23:28 +0100

    Increase max text message buffer size for websocket connections

--- kurento-group-call/src/main/java/org/kurento/tutorial/groupcall/GroupCallApp.java
+++ kurento-group-call/src/main/java/org/kurento/tutorial/groupcall/GroupCallApp.java
@@ -24,6 +24,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 /**
  *
@@ -52,6 +53,13 @@ public class GroupCallApp implements WebSocketConfigurer {
   @Bean
   public KurentoClient kurentoClient() {
     return KurentoClient.create();
+  }
+
+  @Bean
+  public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+    ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+    container.setMaxTextMessageBufferSize(32768);
+    return container;
   }
 
   public static void main(String[] args) throws Exception {
--- kurento-platedetector/src/main/java/org/kurento/tutorial/platedetector/PlateDetectorApp.java
+++ kurento-platedetector/src/main/java/org/kurento/tutorial/platedetector/PlateDetectorApp.java
@@ -24,6 +24,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 /**
  * Plate Detector main class.
@@ -44,6 +45,13 @@ public class PlateDetectorApp implements WebSocketConfigurer {
   @Bean
   public KurentoClient kurentoClient() {
     return KurentoClient.create();
+  }
+
+  @Bean
+  public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+    ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+    container.setMaxTextMessageBufferSize(32768);
+    return container;
   }
 
   @Override
